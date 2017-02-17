37127fcdb86163d3b9231579c4f138234897b6c6
noah tenser <ntenser@hotmail.com>
2017-02-17 14:24:45 -0500

    loop over candidates

--- election.js
+++ election.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
+$(function() {
+    $.ajax({
+      url: 'https://bb-election-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(data){
+      for (var i = 0; i < data.candidates.length; i++) {
+          console.log(data.candidates[i]);
+        }
 
-  // Imagination!
 
-});
+  });
+  });
