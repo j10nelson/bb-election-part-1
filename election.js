1db8dc23f63f08260d7f73965004b5e3374c281a
noah tenser <ntenser@hotmail.com>
2017-02-17 14:49:50 -0500

    added names and notes

--- election.js
+++ election.js
@@ -1,13 +1,11 @@
 $(function() {
     $.ajax({
       url: 'https://bb-election-api.herokuapp.com/',
-      method: 'GET',
-      dataType: 'json'
-    }).done(function(data){
-      for (var i = 0; i < data.candidates.length; i++) {
-          console.log(data.candidates[i]);
+      method: 'GET'
+    }).done(function(responseData){
+      for (var i = 0; i < responseData.candidates.length; i++) {
+          console.log(responseData.candidates[i]);
+          $( '#candidate' ).append( '<li> Name: ' + responseData.candidates[i].name + '<br> Votes: ' + responseData.candidates[i].votes + '</li>' );
         }
-
-
   });
   });
